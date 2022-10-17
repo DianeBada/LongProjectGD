1c0ad6aded18ff5ecb7aefded2cc05a5ba420242
calebgunner <cmayamona7@gmail.com>
2022-10-17 22:38:58 +0200

    Animations added
    1. Added the animation sprites
    2. Made the animations (they do not work with the controls as of yet)
    3. Made a change to the localScale

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -62,9 +62,9 @@ public class PlayerMovement : MonoBehaviour
 
         //Flip player when moving left-right
         if (horizontalInput > 0.01f)
-            transform.localScale = Vector3.one;
+            transform.localScale = new Vector3(0.7f, 0.7f, 0.7f);
         else if (horizontalInput < -0.01f)
-            transform.localScale = new Vector3(-1, 1, 1);
+            transform.localScale = new Vector3(-0.7f, 0.7f, 0.7f);
 
         //Set animator parameters
         //anim.SetBool("run", horizontalInput != 0);
