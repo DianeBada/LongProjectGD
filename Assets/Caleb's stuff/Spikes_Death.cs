9bcc3fef0dd5518726308442b3906e265751497f
calebgunner <cmayamona7@gmail.com>
2022-10-18 20:49:10 +0200

    Level halfway complete

---
+++ Assets/Caleb's stuff/Spikes_Death.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Spikes_Death : MonoBehaviour
+{
+    public Animator playerAnimator;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            playerAnimator.SetTrigger("player Is Dead"); //Trigger prevents other animations from playing
+
+            StartCoroutine(ReloadScene());
+        }
+    }
+
+    private IEnumerator ReloadScene()
+    {
+        //Wait for 2 seconds
+        yield return new WaitForSeconds(2);
+
+        SceneManager.LoadScene("scene"); //reload scene
+    }
+}
